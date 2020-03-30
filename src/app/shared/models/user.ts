56bda4a00531a326819abf6aec0f2886c79f5bfe
Marineincera <marine.incera@gmail.com>
2020-03-30 18:16:16 +0200

    feat(shared): user class initialized

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,2 +1,25 @@
+import { Item } from './item';
+import { Loan } from './loan';
+import { Opinion } from './opinion';
+
 export class User {
+    id: number;
+    avatar?: string;
+    pseudo: string;
+    email: string;
+    password: string;
+    city: string;
+    registrationDate?: Date;
+    walkingDelivery?: boolean;
+    letterDelivery?: boolean;
+    items?: Array<Item>;
+    borrows?: Array<Loan>;
+    loans?: Array<Loan>;
+    writtenOpinions?: Array<Opinion>;
+    receivedOpinions?: Array<Opinion>;
+
+    constructor(user: User){
+        Object.assign(user, this)
+    }
+
 }

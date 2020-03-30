6a8657bba862bec4e9033b305f015ec2bd0c2fe3
Marineincera <marine.incera@gmail.com>
2020-03-30 17:53:18 +0200

    feat(models): category class initialized

--- src/app/shared/models/category.ts
+++ src/app/shared/models/category.ts
@@ -1,2 +1,12 @@
+import { Item } from './item';
+
 export class Category {
+    id: number;
+    name: string;
+    creationDate?: Date;
+    items: Array<Item>;
+
+    constructor(category: Category){
+        Object.assign(category, this)
+    }
 }

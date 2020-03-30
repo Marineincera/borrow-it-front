f00c4eeaf6402d9364dcfb0f08d36ed1035588e0
Marineincera <marine.incera@gmail.com>
2020-03-30 18:07:37 +0200

    feat(shared): loan class initialized

--- src/app/shared/models/loan.ts
+++ src/app/shared/models/loan.ts
@@ -1,2 +1,16 @@
+import { Item } from './item';
+import { User } from './user';
+import { LoanStatus } from './loan-status';
+
 export class Loan {
+    id: number;
+    borrowedItem: Item;
+    borrower: User;
+    owner: User;
+    borrowDate?: Date;
+    loanStatus?: LoanStatus;
+
+    constructor(loan: Loan){
+        Object.assign(loan, this)
+    }
 }

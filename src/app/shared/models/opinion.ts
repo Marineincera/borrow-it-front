7d4de7d57db9ad89229cddc237d3b5e1b225d6c1
Marineincera <marine.incera@gmail.com>
2020-03-30 18:08:09 +0200

    feat(shared): opinion class initialized

--- src/app/shared/models/opinion.ts
+++ src/app/shared/models/opinion.ts
@@ -1,2 +1,12 @@
+import { User } from './user';
+
 export class Opinion {
+    id: number;
+    updateDate?: Date;
+    author: User;
+    addressee: User;
+
+    constructor(opinion: Opinion){
+        Object.assign(opinion, this)
+    }
 }

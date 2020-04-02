223e463c2c1dbb4bcb8e50e373e436045da040aa
Marineincera <marine.incera@gmail.com>
2020-04-02 23:11:46 +0200

    feat(components): homepage - items & ojects lists ok

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -13,32 +13,45 @@ export class ListComponent implements OnInit {
   constructor(private itemService: ItemService) { }
 
   @Input()
-  itemsToDisplay : Item[];
-  items: Array<Item> = [];
-  users:  Array<User>;
+  arrayToDisplay: Array<any>;
+
+  array: Array<any> = [];
+  users = false;
+  title : string;
   filter : number;
   
   ngOnInit(): void {
+    this.determineTypeOfArray(this.arrayToDisplay);
     this.filter = 0;
-    this.initializeArray(this.itemsToDisplay);
+    this.initializeArray(this.arrayToDisplay);
+  }
+
+  determineTypeOfArray(array){
+    if(Object.getOwnPropertyDescriptor(array[0], 'pseudo')){
+    this.users = true;
+    this.title = "Membres";
+    }
+    if(Object.getOwnPropertyDescriptor(array[0], 'note')){
+      this.title = "Objets Disponibles"
+    }
   }
 
   initializeArray(arrayParam: Array<any>) {
     this.filter = this.filter + 4;
-    if(this.itemsToDisplay){
+    if(this.arrayToDisplay){
      for(let i = this.filter - 4; i < this.filter; i++){
-       this.items.push(this.itemsToDisplay[i]);
+       this.array.push(this.arrayToDisplay[i]);
      }
-      console.log(this.items)
+      console.log(this.array)
     }
   }
   
-  addItemsToList(array){
+  addElementsToList(array){
     this.initializeArray(array)
   }
 
   closeList(array: Array<any>){
-    array.splice(4,(array.length-4))
+    array.splice(0,array.length)
     this.ngOnInit()
   }
   

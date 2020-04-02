6265bba0aba642471ac67e6547fec2dfd00fbed3
Marineincera <marine.incera@gmail.com>
2020-04-02 23:06:43 +0200

    feat(service): user service done

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { WshelperService } from './wshelper.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  static URL = 'http://localhost:3000/users'
+
+  constructor(private service: WshelperService) { }
+
+  getAllUsers() {
+    return this.service.get(UserService.URL );
+  }
+
+  postUser(user: User) {
+    return this.service.post(UserService.URL, User);
+  }
+
+  getOneUser(id: number) {
+    return this.service.get(UserService.URL + id.toString());
+  }
+
+  delete(id) {
+    return this.service.delete(UserService.URL + 'delete/' + id);
+  }
+
+  update(id, user) {
+    return this.service.put(UserService.URL + 'modify/' + id, user);
+  }
 }

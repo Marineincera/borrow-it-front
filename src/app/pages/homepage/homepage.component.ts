748a4b1c9d74c239baa0d79ed1c7df9e3d4b7ab2
Marineincera <marine.incera@gmail.com>
2020-04-02 23:10:36 +0200

    feat(pages): homepage - items&users arrays initialized

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from 'src/app/shared/models/item';
 import { ItemService } from 'src/app/shared/services/item.service';
+import { UserService } from 'src/app/shared/services/user.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent implements OnInit{
 
-  constructor(private itemService: ItemService) { }
+  constructor(private itemService: ItemService, private _userService: UserService) { }
 
   items: Array<Item>;
+  users: Array<User>;
 
 
   ngOnInit(): void {
     this.initializeItemsArray();
+    this.initializeUsersArray();
   }
 
   initializeItemsArray(){
@@ -25,21 +29,13 @@ export class HomepageComponent implements OnInit {
     })
   }
 
-  // this.itemService.getItemFilter(4 * this.userClicks).subscribe((data: Array<Item>) => {
-  //   this.items = data
-  //   console.log(this.items)
+  initializeUsersArray(){
+    this._userService.getAllUsers().subscribe((data: Array<User>) => {
+  this.users = data
+  console.log(this.users)
+})
+}
 
-  // addItemsToList(){
-  //   this.userClicks = this.userClicks + 1;
-  //   this.itemService.getItemFilter(4 * this.userClicks).subscribe((data: Array<Item>) => {
-  //     this.items = data
-  //     console.log(this.userClicks)
-  //   })
-  // }
 
-  // closeList(){
-  //   this.userClicks = 0;
-  //   this.addItemsToList()
-  // }
 
 }

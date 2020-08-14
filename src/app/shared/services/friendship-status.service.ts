099492adda5faeec3e8fe956e4ea0f1003fe127e
Marine Incera <marine.incera@gmail.com>
2020-08-14 02:49:23 +0200

    feat(services): friendship and friendships-status done

--- src/app/shared/services/friendship-status.service.ts
+++ src/app/shared/services/friendship-status.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { WshelperService } from "./wshelper.service";
+import { FriendshipService } from "./friendship.service";
+import { Friendship } from "../models/friendship";
+import { FriendshipStatus } from "../models/friendship-status";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FriendshipStatusService {
+  static URL = "http://localhost:3000/friendshipstatus";
 
-  constructor() { }
+  constructor(private service: WshelperService) {}
+
+  getAll() {
+    return this.service.get(FriendshipStatusService.URL);
+  }
+
+  post(friendshipStatus: FriendshipStatus) {
+    return this.service.post(FriendshipStatusService.URL, friendshipStatus);
+  }
+
+  getOne(id: number) {
+    return this.service.get(FriendshipStatusService.URL + "/" + id.toString());
+  }
+
+  delete(id) {
+    return this.service.delete(FriendshipStatusService.URL + "delete/" + id);
+  }
+
+  update(id, friendshipStatus) {
+    return this.service.put(
+      FriendshipStatusService.URL + "update/" + id,
+      friendshipStatus
+    );
+  }
 }
--- src/app/shared/services/friendship.service.ts
+++ src/app/shared/services/friendship.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Friendship } from "../models/friendship";
+import { WshelperService } from "./wshelper.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FriendshipService {
+  static URL = "http://localhost:3000/friendship";
 
-  constructor() { }
+  constructor(private service: WshelperService) {}
+
+  getAll() {
+    return this.service.get(FriendshipService.URL);
+  }
+
+  post(friendship: Friendship) {
+    return this.service.post(FriendshipService.URL, friendship);
+  }
+
+  getOne(id: number) {
+    return this.service.get(FriendshipService.URL + "/" + id.toString());
+  }
+
+  delete(id) {
+    return this.service.delete(FriendshipService.URL + "delete/" + id);
+  }
+
+  update(id, friendship) {
+    return this.service.put(FriendshipService.URL + "update/" + id, friendship);
+  }
 }

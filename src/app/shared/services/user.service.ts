45263c4d92f271a232e00e69a47eb5b848e3426b
Marine Incera <marine.incera@gmail.com>
2020-09-06 15:05:46 +0200

    feat(profil-picture): defaul picture if null

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -114,6 +114,9 @@ export class UserService {
     return this.http.get(UserService.URL + "users/search/me").pipe(
       tap(async (user: User) => {
         this.connectedUser = user;
+        if (user.avatar === null) {
+          user.avatar = "../../../assets/profil-picture-null.png";
+        }
         this.loans = user.loans;
         const loansCategories = await this.determineLoansCategories(user);
         const borrowsCategories = await this.determineBorrowsCategories(user);
